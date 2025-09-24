7116b8b3dcc234ddd930378375778afbcd48fb31
Ozy Prazuganda <OzyPrazuganda75@gmail.com>
2025-09-24 18:07:20 +0700

    progressing on strat validator

--- dashboard_dev.py
+++ dashboard_dev.py
@@ -26,9 +26,60 @@ st.set_page_config(layout="wide")
 team = st.sidebar.radio('Team', ['QC'])
 
 if team == 'QC':
-    # st.markdown('#####')
+    page = st.sidebar.selectbox("Pages", ['Performance'])
 
-    page = st.sidebar.selectbox("Pages", ['Validator'])
+    if page == 'Performance':
+        df_sampling = pd.read_csv('dataset_qc/kalib_sampling.csv', parse_dates=['Tanggal Sampling'])
+        
+        # Filter
+        df_sampling = df_sampling[df_sampling['Agent Sampling'] != 'No Data']
+        validator = st.sidebar.radio('Validators', df_sampling['Agent Sampling'].unique())
 
-    if page == 'Validator':
-        st.title("Validator Stats")
+        # Custom CSS for card style
+        st.markdown("""
+            <style>
+            .card {
+                background-color: #ffffff;
+                border-radius: 12px;
+                padding: 20px;
+                box-shadow: 0px 2px 10px rgba(0,0,0,0.1);
+                margin: 10px;
+                text-align: center;
+            }
+            .card img {
+                border-radius: 8px;
+                max-width: 100%;
+            }
+            </style>
+        """, unsafe_allow_html=True)
+
+        cols = st.columns([2, 4, 4, 4])
+
+        with cols[0]:
+            if validator == 'Aulia':
+                st.image('pict/gawr_gura.png', width=200)
+
+        with cols[1]:
+            st.markdown(f"""
+                <div class="card", style="text-align: left;">
+                    <h3>{validator}</h3>
+                    <p>Quality Control Specialist</p>
+                </div>
+            """, unsafe_allow_html=True)
+
+        with cols[2]:
+            st.markdown("""
+                <div class="card">
+                    test
+                </div>
+            """, unsafe_allow_html=True)
+
+            #Accuracy Validator
+            
+
+        with cols[3]:
+            st.markdown("""
+                <div class="card">
+                    test
+                </div>
+            """, unsafe_allow_html=True)
